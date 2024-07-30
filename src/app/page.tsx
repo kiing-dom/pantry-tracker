2eb05d66ab92f43f76edd8e921eb440a470cd811
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-07-30 17:55:04 +0100

    Firebase Connected/Functionality: added addItem functionality, fetchItems, deleteItem | need to add updateItem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,63 @@
 "use client"
 
 import React, { useEffect, useState } from 'react';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, deleteDoc, query, onSnapshot, doc } from 'firebase/firestore';
+import { db } from '../../firebaseConfig';
 
 type Item = {
+  id: string,
   name: string,
   quantity: string | number
 }
 
+
+
 export default function Home() {
   const [items, setItems] = useState<Item[]>([
-    { name: "Bread Slice", quantity: 2 },
-    { name: "Pasta", quantity: 4 },
-    { name: "Sugar", quantity: 2 }
 
   ]);
-  const [newItem, setNewItem] = useState<{name:string, quantity:string}>({ name: '', quantity: '' })
-
+  const [newItem, setNewItem] = useState<{ name: string, quantity: string }>({ name: '', quantity: '' })
+  
   const [total, setTotal] = useState(0);
+  
+  // read items from database
+  useEffect(() => {
+    const fetchData = async () => {
+      const q = query(collection(db, 'items'));
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const itemsArray: Item[] = [];
+        querySnapshot.forEach((doc) => {
+          itemsArray.push({ ...doc.data(), id: doc.id } as Item);
+        });
+        setItems(itemsArray);
+        const totalQuantity = itemsArray.reduce((acc, item) => acc + Number(item.quantity), 0);
+        setTotal(totalQuantity);
+      });
+      return unsubscribe;
+    };
+
+    fetchData().catch(console.error);
+  }, []);
 
 
   // add items to database
   const addItem = async (ev: React.FormEvent) => {
     ev.preventDefault()
-    if(newItem.name !== '' && newItem.quantity !== '') {
-      setItems([...items, newItem]);
-      setNewItem({name: '', quantity: ''});
+    if (newItem.name !== '' && newItem.quantity !== '') {
+      // setItems([...items, newItem]);
+
+      await addDoc(collection(db, 'items',), {
+        name: newItem.name.trim(),
+        quantity: newItem.quantity
+      })
+      setNewItem({ name: '', quantity: '' });
     }
   }
 
-  // read items from database
-
-
   // delete items from database
-
+  const deleteItem = async (id: string) => {
+    await deleteDoc(doc(db, 'items', id));
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -41,28 +65,28 @@ export default function Home() {
         <h1 className="text-3xl text-center">Pantry Tracker</h1>
         <div className="bg-neutral-400 p-4 rounded-lg">
           <form className="grid grid-cols-4 items-center text-black">
-            <input 
-              className="col-span-2 p-3 border rounded-lg drop-shadow-lg" 
-              type="text" 
+            <input
+              className="col-span-2 p-3 border rounded-lg drop-shadow-lg"
+              type="text"
               placeholder="Enter Item"
-              value={newItem.name} 
-              onChange={(e) => setNewItem({...newItem, name: e.target.value})}
+              value={newItem.name}
+              onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
             />
 
-            <input 
-              className="col-span-1 p-3 border rounded-lg mx-3 drop-shadow-lg" 
-              type="number" 
-              placeholder="Enter Quantity" 
+            <input
+              className="col-span-1 p-3 border rounded-lg mx-3 drop-shadow-lg"
+              type="number"
+              placeholder="Enter Quantity"
               value={newItem.quantity}
-              onChange={(e) => setNewItem({...newItem, quantity: e.target.value})} 
+              onChange={(e) => setNewItem({ ...newItem, quantity: e.target.value })}
             />
 
-            <button 
-              className="bg-slate-700 rounded-md drop-shadow-lg hover:scale-110 hover:bg-slate-400 transition-transform w-24 h-12 text-2xl" 
+            <button
+              className="bg-slate-700 rounded-md drop-shadow-lg hover:scale-110 hover:bg-slate-400 transition-transform w-24 h-12 text-2xl"
               type="submit"
               onClick={addItem}
-              >
-                +
+            >
+              +
             </button>
           </form>
 
@@ -73,7 +97,7 @@ export default function Home() {
                   <span className='capitalize'>{item.name}</span>
                   <span className='text-yellow-300'>x{item.quantity}</span>
                 </div>
-                <button className='ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900'>X</button>
+                <button onClick={() => deleteItem(item.id)} className='ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900'>X</button>
               </li>
             ))}
           </ul>
